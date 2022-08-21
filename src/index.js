6722b34443a524784c12a53e021b3852eccb0cab
carlosdcm <carloschoqque123@gmail.com>
2022-08-21 16:24:17 -0500

    modified

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import app from './app.js'
 const port = app.get('PORT')
 import './db.js'
 app.listen(port)
-console.log(`Server running on http://localhost:${port}`)
+console.log(`Server running on port ${port}`)
